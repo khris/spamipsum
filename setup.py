c3a4deb255e0be88b9dda82510eaa93174769d8f
khris <khris@khrislog.net>
2013-12-08 16:50:19 +0900

    Add `test` command to `setup.py` and use `setuptools`
    Instead of `distutils`. And etc.

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
-from distutils.core import setup
+import sys
+
+from setuptools import find_packages
+from setuptools import setup
+from setuptools.command.test import test
+
+
+class PyTest(test):
+    def finalize_options(self):
+        test.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 setup(
     name='SpamIpsum',
     version='0.1dev',
-    packages=['spamipsum'],
+    packages=find_packages(exclude=['tests']),
     url='https://github.com/khris/spamipsum',
     license='MIT Licence',
     author='Hong Segi',
-    author_email='khris@khrislog.net',
-    description='Placeholder for ugly real world'
+    author_email='khris' '@' 'khrislog.net',
+    description='Placeholder for ugly real world',
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest}
 )
